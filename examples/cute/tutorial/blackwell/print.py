6827d45551b5ebce37cf05b8101c529b78cc2b23
zhutaoyu <zhutaoyu97@gmail.com>
2025-11-17 13:52:13 +0800

    add files

---
+++ examples/cute/tutorial/blackwell/print.py
@@ -0,0 +1,6 @@
+import os
+os.system("pwd")
+os.system("mkdir profile_data")
+# os.system("ls -lh")
+os.system("git clone https://github.com/benenzhu/cutlass.git --depth=1")
+os.system("cd cutlass/ && examples/cute/tutorial/blackwell && nvcc 01_mma_sm100.cu -o 01_mma_sm100")
