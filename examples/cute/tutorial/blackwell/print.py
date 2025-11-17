f7c34dc48614548ed7c79bb3eee7b27437d4dd9b
zhutaoyu <zhutaoyu97@gmail.com>
2025-11-17 13:58:38 +0800

    refact

--- examples/cute/tutorial/blackwell/print.py
+++ examples/cute/tutorial/blackwell/print.py
@@ -1,6 +1,9 @@
 import os
-os.system("pwd")
-os.system("mkdir profile_data")
-os.system("ls -lh")
-os.system("git clone https://github.com/benenzhu/cutlass.git --depth=1 cutlass2")
-os.system("cd cutlass2/examples/cute/tutorial/blackwell && nvcc 01_mma_sm100.cu -o 01_mma_sm100")
+if not os.path.exists("cutlass2"):
+    os.system("pwd")
+    os.system("mkdir profile_data")
+    os.system("ls -lh")
+    os.system("git clone https://github.com/benenzhu/cutlass.git --depth=1 cutlass2")
+    os.system("cd cutlass2/examples/cute/tutorial/blackwell && nvcc 01_mma_sm100.cu -o 01_mma_sm100 && ./a.out")
+else:
+    print('running for 2nd time here...')
