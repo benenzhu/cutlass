a5896fb1458d4e9825af7f88dfc2265cf0bbdac1
zhutaoyu <zhutaoyu97@gmail.com>
2025-11-17 13:56:32 +0800

    refact

--- examples/cute/tutorial/blackwell/print.py
+++ examples/cute/tutorial/blackwell/print.py
@@ -2,5 +2,5 @@ import os
 os.system("pwd")
 os.system("mkdir profile_data")
 os.system("ls -lh")
-os.system("git clone https://github.com/benenzhu/cutlass.git --depth=1")
-os.system("cd cutlass/ &&cd examples/cute/tutorial/blackwell && nvcc 01_mma_sm100.cu -o 01_mma_sm100")
+os.system("git clone https://github.com/benenzhu/cutlass.git --depth=1 cutlass2")
+os.system("cd cutlass2/examples/cute/tutorial/blackwell && nvcc 01_mma_sm100.cu -o 01_mma_sm100")
